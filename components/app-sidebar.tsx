8f0c2a4cc557bf2f711547edab820166f006cfb5
MiguelHort <miguel.d.hort@gmail.com.com>
2025-01-31 23:15:32 -0300

    Sidebar Icons ajuste

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -37,7 +37,7 @@ const items = [
 
 export function AppSidebar() {
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Menu</SidebarGroupLabel>
